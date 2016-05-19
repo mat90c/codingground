93091faada70ac55d32486880bb7529200c99dcd
mat90c <mat90c@gmail.com>
2016-05-19 15:50:01 +0200

    nuovo algoritmo

--- New Project-20160518/HelloWorld.java
+++ New Project-20160518/HelloWorld.java
@@ -13,19 +13,37 @@ public class HelloWorld{
         
         ArrayList<ActivityData> data = sleep.loadData(RawData1.part1+RawData2.part2+RawData3.part3);
         final int start = 1500;
-        double categoryAvg = 0;
-        double intensityAvg = 0;
-        double stepsAvg = 0;
+		
+        double Distance = 0;
+        double max = 0;
+		
         for(int i=start;i<start+800;i++)
         {
-            if(i>3)
+			//Distance è il numero di 0 della variabile intensity in una finestra temporale di dimensione 5 
+			//Max è il massimo della variabile intensity in una finestra temporale di dimensione 5 
+            if(i>5)
             {
-                categoryAvg = (data.get(i).getCategory() + data.get(i-1).getCategory() + data.get(i-2).getCategory()) / 3.0;
-                intensityAvg = (data.get(i).getIntensity() + data.get(i-1).getIntensity() + data.get(i-2).getIntensity()) / 3.0;
-                stepsAvg = (data.get(i).getSteps() + data.get(i-1).getSteps() + data.get(i-2).getSteps()) / 3.0;
+				Distance = 0;
+				if(data.get(i).getIntensity()==0)
+					Distance++;
+				if(data.get(i-1).getIntensity()==0)
+					Distance++;
+				if(data.get(i-2).getIntensity()==0)
+					Distance++;
+				if(data.get(i-3).getIntensity()==0)
+					Distance++;
+				if(data.get(i-4).getIntensity()==0)
+					Distance++;
+				
+				max=0;
+				max = Math.max(max,data.get(i).getIntensity());
+				max = Math.max(max,data.get(i-1).getIntensity());
+				max = Math.max(max,data.get(i-2).getIntensity());
+				max = Math.max(max,data.get(i-3).getIntensity());
+				max = Math.max(max,data.get(i-4).getIntensity());
             }
             ActivityData d = data.get(i);
-            System.out.println(sdf.format(d.getTimestamp()*1000)+" "+d.getTimestamp()+";"+d.getCategory()+";"+d.getIntensity()+";"+d.getSteps()+" = "+sleep.getMode(Math.round(categoryAvg),intensityAvg,stepsAvg));
+            System.out.println(sdf.format(d.getTimestamp()*1000)+" "+d.getTimestamp()+";"+d.getCategory()+";"+d.getIntensity()+";"+d.getSteps()+" = "+sleep.getMode(d.getCategory(),d.getIntensity(),d.getSteps(), Distance, max));
         }
      }
 }
--- New Project-20160518/Sleep.java
+++ New Project-20160518/Sleep.java
@@ -32,70 +32,201 @@ public class Sleep
     } 
 	
 	
-	public int getMode(int category, int intensity, int steps)
+	public int getMode(int category, int intensity, int steps, double Distance, double max)
 	{
-		if (intensity <= 3.333333) {
-    if (intensity <= 0) {
-        if (steps <= 0) return 0;
-        if (steps > 0) {
-            if (steps <= 35) {
-                if (steps <= 0.666667) return 1;
-                if (steps > 0.666667) return 0;
-            }
-            if (steps > 35) return 0;
-        }
-    }
-    if (intensity > 0) return 0;
-}
-if (intensity > 3.333333) {
-    if (steps <= 1) {
-        if (category == 0) return 0;
-        if (category == 1) return 0;
-        if (category == 2) return 0;
-        if (category == 3) return 0;
-        if (category == 4) return 0;
-        if (category == 5) {
-            if (steps <= 0) return 0;
-            if (steps > 0) {
-                if (steps <= 0.333333) return 2;
-                if (steps > 0.333333) return 1;
-            }
-        }
-        if (category == 6) return 0;
-        if (category == 7) return 0;
-        if (category == 9) return 0;
-        if (category == 11) return 0;
-        if (category == 15) return 0;
-    }
-    if (steps > 1) {
-        if (steps <= 33) {
-            if (steps <= 21) return 1;
-            if (steps > 21) {
-                if (category == 0) return 1;
-                if (category == 1) return 1;
-                if (category == 2) return 1;
-                if (category == 3) return 1;
-                if (category == 4) {
-                    if (steps <= 22.333333) return 0;
-                    if (steps > 22.333333) return 1;
+	if (category == 0) {
+    if (Distance <= 0) return 0;
+    if (Distance > 0) {
+        if (Distance <= 4) {
+            if (max <= 52) {
+                if (intensity <= 21) {
+                    if (max <= 27) {
+                        if (Distance <= 2) {
+                            if (Distance <= 1) {
+                                if (intensity <= 0) return 0;
+                                if (intensity > 0) {
+                                    if (intensity <= 3) {
+                                        if (max <= 16) return 0;
+                                        if (max > 16) return 1;
+                                    }
+                                    if (intensity > 3) return 0;
+                                }
+                            }
+                            if (Distance > 1) return 0;
+                        }
+                        if (Distance > 2) {
+                            if (Distance <= 3) {
+                                if (max <= 2) return 0;
+                                if (max > 2) return 1;
+                            }
+                            if (Distance > 3) {
+                                if (intensity <= 0) return 1;
+                                if (intensity > 0) return 0;
+                            }
+                        }
+                    }
+                    if (max > 27) {
+                        if (max <= 35) return 1;
+                        if (max > 35) {
+                            if (Distance <= 1) return 0;
+                            if (Distance > 1) return 1;
+                        }
+                    }
                 }
-                if (category == 5) return 1;
-                if (category == 6) return 0;
-                if (category == 7) return 1;
-                if (category == 9) return 0;
-                if (category == 11) return 1;
-                if (category == 15) return 1;
+                if (intensity > 21) {
+                    if (max <= 27) return 0;
+                    if (max > 27) {
+                        if (max <= 31) {
+                            if (max <= 30) {
+                                if (intensity <= 27) return 1;
+                                if (intensity > 27) {
+                                    if (Distance <= 2) return 0;
+                                    if (Distance > 2) {
+                                        if (Distance <= 3) return 1;
+                                        if (Distance > 3) return 0;
+                                    }
+                                }
+                            }
+                            if (max > 30) return 1;
+                        }
+                        if (max > 31) {
+                            if (steps <= 5) return 0;
+                            if (steps > 5) {
+                                if (Distance <= 1) return 1;
+                                if (Distance > 1) {
+                                    if (max <= 50) {
+                                        if (max <= 44) {
+                                            if (Distance <= 2) return 0;
+                                            if (Distance > 2) return 1;
+                                        }
+                                        if (max > 44) return 0;
+                                    }
+                                    if (max > 50) return 1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (max > 52) {
+                if (Distance <= 2) return 0;
+                if (Distance > 2) {
+                    if (intensity <= 29) {
+                        if (max <= 59) return 1;
+                        if (max > 59) {
+                            if (max <= 67) return 0;
+                            if (max > 67) return 1;
+                        }
+                    }
+                    if (intensity > 29) return 0;
+                }
             }
         }
-        if (steps > 33) {
-            if (steps <= 42.666667) {
-                if (steps <= 35) return 0;
-                if (steps > 35) return 3;
-            }
-            if (steps > 42.666667) return 0;
-        }
+        if (Distance > 4) return 0;
     }
 }
+if (category == 1) {
+    if (max <= 73) {
+        if (Distance <= 1) return 0;
+        if (Distance > 1) return 1;
+    }
+    if (max > 73) return 0;
+}
+if (category == 3) return 0;
+if (category == 4) {
+    if (Distance <= 0) return 0;
+    if (Distance > 0) {
+        if (max <= 19) return 0;
+        if (max > 19) return 1;
+    }
+}
+if (category == 5) {
+    if (Distance <= 4) {
+        if (Distance <= 3) {
+            if (max <= 15) {
+                if (max <= 1) return 0;
+                if (max > 1) {
+                    if (Distance <= 1) return 0;
+                    if (Distance > 1) {
+                        if (max <= 3) {
+                            if (Distance <= 2) {
+                                if (intensity <= 2) {
+                                    if (intensity <= 0) return 1;
+                                    if (intensity > 0) return 0;
+                                }
+                                if (intensity > 2) return 1;
+                            }
+                            if (Distance > 2) {
+                                if (intensity <= 0) return 0;
+                                if (intensity > 0) {
+                                    if (intensity <= 1) return 1;
+                                    if (intensity > 1) return 0;
+                                }
+                            }
+                        }
+                        if (max > 3) return 1;
+                    }
+                }
+            }
+            if (max > 15) {
+                if (Distance <= 0) {
+                    if (intensity <= 2) return 1;
+                    if (intensity > 2) {
+                        if (intensity <= 12) {
+                            if (max <= 40) {
+                                if (max <= 24) return 0;
+                                if (max > 24) {
+                                    if (intensity <= 4) return 1;
+                                    if (intensity > 4) return 0;
+                                }
+                            }
+                            if (max > 40) return 0;
+                        }
+                        if (intensity > 12) {
+                            if (max <= 41) return 0;
+                            if (max > 41) return 1;
+                        }
+                    }
+                }
+                if (Distance > 0) {
+                    if (max <= 30) {
+                        if (max <= 28) {
+                            if (max <= 19) {
+                                if (max <= 18) return 1;
+                                if (max > 18) return 0;
+                            }
+                            if (max > 19) return 1;
+                        }
+                        if (max > 28) {
+                            if (intensity <= 12) return 0;
+                            if (intensity > 12) return 1;
+                        }
+                    }
+                    if (max > 30) return 1;
+                }
+            }
+        }
+        if (Distance > 3) return 1;
+    }
+    if (Distance > 4) return 0;
+}
+if (category == 6) return 0;
+if (category == 11) {
+    if (max <= 20) return 0;
+    if (max > 20) {
+        if (Distance <= 0) return 0;
+        if (Distance > 0) return 1;
+    }
+}
+if (category == 12) return 0;
+if (category == 16) return 0;
+if (category == 17) return 0;
+if (category == 19) return 0;
+if (category == 21) return 1;
+if (category == 28) return 0;
+if (category == 33) return 0;
+if (category == 44) return 0;
+
 		
 		return 0;
 	}
